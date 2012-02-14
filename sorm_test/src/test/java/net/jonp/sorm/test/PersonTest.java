b68c382f3d8fdd22e0b63bd1524334cb965673c7
Jonathan Pearson <celloman@gmail.com>
2012-02-14 11:22:23 -0500

    Add more test cases.
    The testMultipleUpdate() case is present but unimplemented, with a fail()
    message.

--- sorm_test/src/test/java/net/jonp/sorm/test/PersonTest.java
+++ sorm_test/src/test/java/net/jonp/sorm/test/PersonTest.java
@@ -1,9 +1,13 @@
 package net.jonp.sorm.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.HashSet;
 
 import net.jonp.sorm.CacheMode;
@@ -59,23 +63,131 @@ public class PersonTest
         final SormSession session = context.getTransientSession(CacheMode.Immediate);
 
         try {
-            testSingleInsertImpl(session);
+            final Person person = testSingleInsertImpl(session);
+
+            // XXX: By holding onto a strong reference to the Person in the
+            // cache, this should prevent the garbage collector from clearing it
+            // out
+            // However, it is still possible, since the key in the weak hash map
+            // is a boxed Integer, and may be cleared anyway
+            assertNotNull(session.cacheGet(Person.class, person.getId()));
         }
         finally {
             session.close();
         }
     }
 
-    private void testSingleInsertImpl(final SormSession session)
+    /**
+     * Insert a person, then test that reading the person with that ID matches.
+     * 
+     * @param session The session to use.
+     * @return The original person that was inserted.
+     * @throws SQLException If there was a problem.
+     */
+    private Person testSingleInsertImpl(final SormSession session)
         throws SQLException
     {
         final Person person = buildSimpleObjects(1)[0];
         Person.Orm.create(session, person);
 
         final Person test = Person.Orm.read(session, person.getId());
-        assertEquals(person.getName(), test.getName());
-        assertEquals(person.getGender(), test.getGender());
-        assertEquals(person.getDob(), test.getDob());
+
+        assertPersonEquals(person, test);
+
+        return person;
+    }
+
+    @Test
+    public void testMultipleInsert()
+        throws SQLException
+    {
+        final SormSession session = context.getSession(CacheMode.None);
+        final Person[] people = buildSimpleObjects(100);
+
+        Person.Orm.create(session, people);
+
+        final Integer keys[] = new Integer[people.length];
+        for (int i = 0; i < keys.length; i++) {
+            keys[i] = people[i].getId();
+        }
+
+        final Collection<Person> testCollection = Person.Orm.read(session, keys);
+        final Person test[] = testCollection.toArray(new Person[testCollection.size()]);
+
+        for (int i = 0; i < people.length; i++) {
+            assertPersonEquals(people[i], test[i]);
+        }
+    }
+
+    @Test
+    public void testSingleUpdate()
+        throws SQLException
+    {
+        final SormSession session = context.getTransientSession(CacheMode.None);
+
+        try {
+            final Person person = populate(1)[0];
+
+            final java.sql.Date today = new java.sql.Date(new java.util.Date().getTime());
+
+            person.setName("newname");
+            person.setGender("trans");
+            person.setDob(today);
+            Person.Orm.update(session, person);
+
+            final Person test = Person.Orm.read(session, person.getId());
+            assertEquals("newname", test.getName());
+            assertEquals("trans", test.getGender());
+            assertEquals(today, test.getDob());
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void testMultipleUpdate()
+    {
+        fail("Not implemented");
+    }
+
+    @Test
+    public void testSingleDelete()
+        throws SQLException
+    {
+        final SormSession session = context.getTransientSession(CacheMode.None);
+
+        try {
+            final Person person = populate(1)[0];
+
+            Person.Orm.delete(session, person);
+
+            final Person test = Person.Orm.read(session, person.getId());
+            assertNull(test);
+        }
+        finally {
+            session.close();
+        }
+    }
+
+    @Test
+    public void testMultipleDelete()
+        throws SQLException
+    {
+        final SormSession session = context.getTransientSession(CacheMode.Immediate);
+
+        try {
+            final Person[] people = populate(100);
+            Person.Orm.delete(session, people);
+
+            for (final Person person : people) {
+                final Person test = Person.Orm.read(session, person.getId());
+                assertNull(test);
+            }
+        }
+        finally {
+            session.close();
+        }
     }
 
     /**
@@ -183,5 +295,12 @@ public class PersonTest
         }
 
         return objs;
+    }
+
+    private void assertPersonEquals(final Person expected, final Person actual)
+    {
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getGender(), actual.getGender());
+        assertEquals(expected.getDob(), actual.getDob());
     }
 }
